cc1a369053efadbb5a190aa66e8f197870381099
markcarey <mark@markcarey.com>
2021-11-07 23:24:56 -0500

    fix

--- dapp/js/dapp.js
+++ dapp/js/dapp.js
@@ -250,34 +250,33 @@ function wrongNetworkModal(ctx){
     </div>
     `;
     return html;
+}
 
-
-    function notify(message) {
-        $.notify({
-            message: message
-         },
-         {
-            type:'primary',
-            allow_dismiss:false,
-            newest_on_top:false ,
-            mouse_over:false,
-            showProgressbar:false,
-            spacing:10,
-            timer:2000,
-            placement:{
-              from:'top',
-              align:'right'
-            },
-            offset:{
-              x:30,
-              y:30
-            },
-            delay:1000 ,
-            z_index:10000,
-            animate:{
-              enter:'animated bounce',
-              exit:'animated bounce'
-          }
-        });
-    }
+function notify(message) {
+    $.notify({
+        message: message
+     },
+     {
+        type:'primary',
+        allow_dismiss:false,
+        newest_on_top:false ,
+        mouse_over:false,
+        showProgressbar:false,
+        spacing:10,
+        timer:2000,
+        placement:{
+          from:'top',
+          align:'right'
+        },
+        offset:{
+          x:30,
+          y:30
+        },
+        delay:1000 ,
+        z_index:10000,
+        animate:{
+          enter:'animated bounce',
+          exit:'animated bounce'
+      }
+    });
 }
