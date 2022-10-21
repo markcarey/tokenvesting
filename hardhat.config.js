af10a9b1040b04a49e764c418aa2dba62fb54c8e
markcarey <mark@markcarey.com>
2022-10-21 09:58:55 -0400

    misc

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-web3");
-const { API_URL, RINKEBY_API_URL, POLYGON_API_URL, PRIVATE_KEY, ETHERSCAN_API_KEY, POLYSCAN_API_KEY } = process.env;
+const { API_URL, RINKEBY_API_URL, POLYGON_API_URL, PRIVATE_KEY, DEPLOYER_PRIV, ETHERSCAN_API_KEY, POLYSCAN_API_KEY } = process.env;
 module.exports = {
   solidity: {
     version: "0.8.0",
@@ -19,14 +19,14 @@ module.exports = {
    defaultNetwork: "mumbai",
    networks: {
       hardhat: {
-        accounts: [{ privateKey: `0x${PRIVATE_KEY}`, balance: "10000000000000000000000"}],
+        accounts: [{ privateKey: `0x${DEPLOYER_PRIV}`, balance: "10000000000000000000000"}],
         forking: {
-          url: API_URL,
-          blockNumber: 20935392  // assumes polygon fork
+          url: POLYGON_API_URL,
+          blockNumber: 31471513  // assumes polygon fork
         },
         loggingEnabled: true,
-        gasMultiplier: 5,
-        gasPrice: 1000000000 * 7
+        //gasMultiplier: 5,
+        //gasPrice: 1000000000 * 7
       },
       mumbai: {
         url: API_URL,
